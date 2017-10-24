789d40c7774656ab9d8859a8d0751eb169b4c6a8
edsmartin7 <edsmartin7@gmail.com>
2017-10-24 01:15:49 -0400

    'second commit'

--- app/src/main/java/com/example/emartin/donaldannoyancegame/MainActivity.java
+++ app/src/main/java/com/example/emartin/donaldannoyancegame/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.emartin.donaldannoyancegame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+//Guess how annoying donald is
 public class MainActivity extends AppCompatActivity {
 
     @Override
