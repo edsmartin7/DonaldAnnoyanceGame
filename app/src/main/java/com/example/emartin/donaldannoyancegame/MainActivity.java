2c0fc57e47abf37fa9e49d0068b54db9af874bcf
edsmartin7 <edsmartin7@gmail.com>
2017-10-26 00:01:48 -0400

    basic functions finished

--- app/src/main/java/com/example/emartin/donaldannoyancegame/MainActivity.java
+++ app/src/main/java/com/example/emartin/donaldannoyancegame/MainActivity.java
@@ -10,17 +10,19 @@ import android.widget.TextView;
 import java.util.Random;
 
 
+//picture
 //Main Screen
 public class MainActivity extends AppCompatActivity {
 
-    private int current_guess;
-    private int random_number;
-    private int current_number;
-    private Button enter_button;
-    private Button new_game;
+    private int currentGuess;
+    private int randomNumber;
+    private int currentNumber;
+    private Button enterButton;
+    private Button newGame;
     private EditText editText;
-    private TextView last_guess;
-    private TextView total_guesses;
+    private TextView previousGuess;
+    private TextView totalGuesses;
+    private TextView clueMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,22 +31,24 @@ public class MainActivity extends AppCompatActivity {
 
         //get a random number between 1 and 100
         Random random_generator = new Random();
-        random_number = random_generator.nextInt(100) + 1;
+        randomNumber = random_generator.nextInt(100) + 1;
+        currentGuess = 10;
 
         editText = (EditText) findViewById(R.id.guessed_number);
-        last_guess = (TextView) findViewById(R.id.last_guess);
-        total_guesses = (TextView) findViewById(R.id.number_of_tries);
+        previousGuess = (TextView) findViewById(R.id.last_guess);
+        totalGuesses = (TextView) findViewById(R.id.number_of_tries);
+        clueMessage = (TextView) findViewById(R.id.clue_message);
 
-        enter_button = (Button) findViewById(R.id.enter_button);
-        enter_button.setOnClickListener(new View.OnClickListener() {
+        enterButton = (Button) findViewById(R.id.enter_button);
+        enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 guessNumber(view);
             }
         });
 
-        new_game = (Button) findViewById(R.id.new_game_button);
-        new_game.setOnClickListener(new View.OnClickListener() {
+        newGame = (Button) findViewById(R.id.new_game_button);
+        newGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 newGame(view);
@@ -55,24 +59,31 @@ public class MainActivity extends AppCompatActivity {
     //called when a user guesses a number
     public void guessNumber(View view) {
 
-        current_number = Integer.parseInt(editText.getText().toString());
+        currentNumber = Integer.parseInt(editText.getText().toString());
 
-        if(current_number == random_number) {
+        if(currentNumber == randomNumber) {
             displayPicture(view, 1);
         } else {
             //last guess, lose game
-            if(current_guess == 0) {
+            if(currentGuess == 0) {
                 displayPicture(view, 0);
                 newGame(view);
             }
 
 
             //change current guess number
-            current_guess--;
+            currentGuess--;
             //change last number chosen
-            last_guess.setText(String.valueOf(current_number));
+            previousGuess.setText(String.valueOf(currentNumber));
             //change number of tries left
-            total_guesses.setText(String.valueOf(current_guess)); //
+            totalGuesses.setText(String.valueOf(currentGuess)); 
+            //give a hint
+            String message = "";
+            if(currentNumber < randomNumber)
+                message = "Incorrect.  Higher than " + currentNumber;
+            else
+                message = "Incorrect.  Lower than " + currentNumber;
+            clueMessage.setText(message);
 
         }
 
@@ -80,7 +91,8 @@ public class MainActivity extends AppCompatActivity {
 
     //called when user presses new game
     public void newGame(View view) {
-        current_guess = 10;
+        //currentGuess = 10;
+        finish();
     }
 
     public void displayPicture(View view, int num) {
